9be95f245c5d2de657a6c6f356ded3e94f80ed15
Arun Kumar <arun4webtech@gmail.com>
2017-08-11 10:43:34 +0530

    getURLParameter function updated

--- JSutility.js
+++ JSutility.js
@@ -1,17 +1,10 @@
-function getParameterByName(name, url) { 
-	if (!url) { 
-		url = window.location.href; 
-	} 
-	name = name.replace(/[\[\]]/g, "\\$&"); 
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), 
-	results = regex.exec(url); 
-	if (!results) 
-		return null; 
-	if (!results[2]) 
-		return ''; 
-	return decodeURIComponent(results[2].replace(/\+/g, " ")); 
+function getURLParameter(name,url) {
+	url = (url==null || url===undefined)?window.location.href:url;
+	url = (url.search(new RegExp("[\?\&]" + name + "=([^\&]*)")) >= 0) ? RegExp.$1 : "";
+	try{
+		return decodeURIComponent(url);
+	}catch(e){return url}
 }
-
 
 
 function isIE()
