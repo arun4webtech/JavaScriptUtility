0af6117d3b624dbbf9a8225c3f0422552e26baad
Arun Kumar <arun4webtech@gmail.com>
2017-09-07 15:35:07 +0530

    old code for getURLParameter

--- JSutility.js
+++ JSutility.js
@@ -5,7 +5,24 @@ function getURLParameter(name,url) {
 		return decodeURIComponent(url);
 	}catch(e){return url}
 }
-
+	/** old code **/
+/*
+function getParameterByName(name, url) 
+{ 
+	if (!url) 
+	{ 
+		url = window.location.href; 
+	} 
+	name = name.replace(/[\[\]]/g, "\\$&"); 
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+	var results = regex.exec(url); 
+	if (!results) 
+		return null; 
+	if (!results[2]) 
+		return ''; 
+	return decodeURIComponent(results[2].replace(/\+/g, " ")); 
+}
+*/
 
 function isIE()
 {
